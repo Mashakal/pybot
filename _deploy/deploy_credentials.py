537746903f92d2edb093d694ede58d8924e23be3
Alex Neuenkirk <t-alneue@microsoft.com>
2016-06-24 08:59:05 -0700

    Created deploy_credentials.py

---
+++ _deploy/deploy_credentials.py
@@ -0,0 +1,9 @@
+from azure.common.credentials import UserPassCredentials
+
+CREDENTIALS = UserPassCredentials(
+    "alex.neuenkirk@alexneuenkirkoutlook.onmicrosoft.com",
+    "aZ36454627%"
+)
+
+SUBSCRIPTION_ID = "91cd8148-679a-49b6-bb4d-c263bc6a11c3"
+
